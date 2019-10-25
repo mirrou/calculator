38ee1cb82bb989b277104afbe6b50ea470b1b639
Maroua Jawadi <marouajawadi@gmail.com>
2019-10-24 19:45:05 -0500

    Co-authored-by: Tatiana Veratti <TatianaVeratti@gmail.com>

--- index.js
+++ index.js
@@ -9,24 +9,53 @@ const add = document.getElementById('add');
 const subtract = document.getElementById('subtract');
 const equals = document.getElementById('equals');
 
+const state = {
+    values:[''], 
+    operator: null
+}
+
+function inputNumber(value){
+
+    const valueIndex = state.values.length -1
+    state.values[valueIndex] += '' + value
+
+    display.innerHTML= state.values[valueIndex]
+}
+
+function inputOperator(value){
+    if(state.values.length > 1 || value === '='){
+        const output = eval(state.values[0] + state.operator + state.values[1]);
+        state.values = [output]
+    }else {state.values.push('')}
+    
+    display.innerHTML = state.values[state.values.length-1];
+    if (value !== '=') state.operator = value;
+}
 //attach event handlers to the number buttons
 numbers.forEach(function(button){ //for each button
     const number = button.innerHTML.toString(); //grab the innerHTML and convert to a string
     //console.log(number)
     button.addEventListener('click', function(){ //listen for the click
-        const value = display.innerHTML + number; //append the number to the display's innerHTML and assign to value
-        display.innerHTML=value; //change innerHTML of display to = value
+        inputNumber(number)
+        // const value = display.innerHTML + number; //append the number to the display's innerHTML and assign to value
+        // display.innerHTML=value; //change innerHTML of display to = value
     })
 })
 
+function resetCalculator() {
+    state.values = [''];
+    state.operator = null;
+    display.innerHTML = state.values;
+  }
+
 //clear button functionality
 clear.addEventListener('click', function(){
-    display.innerHTML = '';
+    resetCalculator()
 })
 
 //negative button functionality
 negative.addEventListener('click', function(){
-    const value = Number(display.innerHTML) * -1;
+    const value = +display.innerHTML * -1;
     display.innerHTML=value;
 })
 
@@ -34,4 +63,16 @@ negative.addEventListener('click', function(){
 percent.addEventListener('click', function(){
     const value = Number(display.innerHTML) / 100;
     display.innerHTML=value;
+})
+
+add.addEventListener('click', function(){
+    inputOperator('+')
+})
+
+subtract.addEventListener('click', function(){
+    inputOperator('-')
+})
+
+equals.addEventListener('click', function(){
+    inputOperator('=')
 })
